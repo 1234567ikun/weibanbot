fc07907938db82a6a87932266e6a3933f6fc803e
JefferyHcool <1063474837@qq.com>
2023-03-11 00:56:04 +0800

    fix数据库断开问题

--- main.py
+++ main.py
@@ -1,93 +1,4 @@
-<<<<<<< HEAD
-import  WeibanAPI
-import time
-import webbrowser
-from rich.console import Console
-from rich.console import Console
-console = Console()
 
-def run():
-
-    console.print('{:=^45}'.format('安全微课逃课助手v3.2'))
-    console.print("程序仅供学习，完全免费")
-    console.print('多学一些安全知识也不是没有用\n')
-    console.print('考试功能已做',style="red on white")
-    console.print("1.5s后自动打开浏览器，需要你 [bold cyan]手动登录 [bold cyan]安全微课")
-    console.print('2.使用学号 [bold cyan]登录 [bold cyan]， [bold red]密码默认是学号后六位 [bold red]')
-    console.print("3.登录以后输入按F12或打开检查，在控制台输入这条命令")
-    console.print("如果卡住请按一下ctrl+c",style="red on white")
-
-    console.print('''data=JSON.parse(localStorage.user);prompt('',data['token']+"&"+data['userId']+"&"+data['tenantCode'])''')
-    with console.status("[bold green]正在打浏览器...") as status:
-            time.sleep(3)
-    webbrowser.open("https://weiban.mycourse.cn/#/")
-    info=input("把刚刚浏览器获取到的信息复制进来并按下回车：\n")
-
-    info=info.split('&')
-    print(info[0],info[1],info[2])
-    data = {"token": info[0], "userId": info[1],
-            "tenantCode": info[2]}
-    userId = data['userId']
-    # print(wb.userId)
-    token = data['token']
-    # # print(wb.token)
-    tenantCode = data['tenantCode']
-    wb = WeibanAPI.WeibanAPI(token=token,userId=userId,tenantCode=tenantCode)
-    print("获取学生信息")
-    wb.getInfo()
-    print('获取学生任务')
-    wb.getTask()
-    wb.getProgress()
-    print('获取课程列表')
-    wb.getCourse()
-    wb.finshiall()
-    print("全部刷完了")
-    console.log("[bold cyan]开始考试[bold cyan]")
-    console.log("不敢保证百分百可以过考试",style="red on white")
-    count=0
-    flag=False
-    while  flag == False:
-        flag=check(wb)
-        print(flag)
-
-    while count<2:
-        count += 1
-        console.log(f"第{count}次尝试")
-
-        try:
-            res=wb.do_paper()
-        except Exception as e:
-            print("没有刷完题，请重新运行补刷",e)
-            input()
-
-        # print(res)
-        wb.get_ques()
-        res=res['data']['score']
-        if res>=90:
-            console.log(f"本次分数为：[bold green]{res}[bold green]")
-            console.log(f"成绩合格")
-            break
-        else:
-            console.log(f"本次分数为：[bold red]{res}[bold red]")
-            console.log(f"成绩不合格")
-
-def check(wb):
-    flag=wb.getProgress()
-    print("未刷完，检查重刷")
-    wb.getInfo()
-    wb.getTask()
-    wb.getProgress()
-    wb.getCourse()
-    wb.finshiall()
-    print(flag)
-    return flag
-
-if __name__ == '__main__':
-    try:
-        run()
-    except KeyboardInterrupt:
-        print("再见~~")
-=======
 import  WeibanAPI
 import time
 import webbrowser
@@ -140,24 +51,68 @@ def run():
     console.log("[bold cyan]开始考试[bold cyan]")
     console.log("不敢保证百分百可以过考试",style="red on white")
     count=0
+    flag=False
+    while  flag == False:
+        flag=check(wb)
+        print(flag)
+
     while count<2:
         count += 1
         console.log(f"第{count}次尝试")
-        res=wb.do_paper()
+
+        try:
+            res=wb.do_paper()
+        except Exception as e:
+            print("错误，输入n重试",e)
+            n=input()
+
         wb.get_ques()
         res=res['data']['score']
         if res>=90:
             console.log(f"本次分数为：[bold green]{res}[bold green]")
             console.log(f"成绩合格")
+            n=input("输入任意键回车结束")
             break
         else:
             console.log(f"本次分数为：[bold red]{res}[bold red]")
             console.log(f"成绩不合格")
+            n=input("是否继续考试y/n")
+            if n=='y':
+                continue
+            else:
+                break
+
+
+
+
+def doPaper(wb,res):
+    wb.get_ques()
+    res = res['data']['score']
+    if res >= 90:
+        console.log(f"本次分数为：[bold green]{res}[bold green]")
+        console.log(f"成绩合格")
+
+        return True
+    else:
+        console.log(f"本次分数为：[bold red]{res}[bold red]")
+        console.log(f"成绩不合格")
+
+
+def check(wb):
+    flag=wb.getProgress()
+    print("未刷完，检查重刷")
+    wb.getInfo()
+    wb.getTask()
+    wb.getProgress()
+    wb.getCourse()
+    wb.finshiall()
+    print(flag)
+    return flag
 
 if __name__ == '__main__':
     try:
         run()
     except KeyboardInterrupt:
         print("再见~~")
->>>>>>> origin/main
+
         time.sleep(5)
--- main.spec
+++ main.spec
@@ -4,7 +4,6 @@
 block_cipher = None
 
 
-<<<<<<< HEAD
 a = Analysis(
     ['main.py'],
     pathex=[],
@@ -44,39 +43,3 @@ exe = EXE(
     entitlements_file=None,
     icon=['university.ico'],
 )
-=======
-a = Analysis(['main.py'],
-             pathex=['C:\\Users\\10634\\Documents\\weibanbot'],
-             binaries=[],
-             datas=[],
-             hiddenimports=[],
-             hookspath=[],
-             hooksconfig={},
-             runtime_hooks=[],
-             excludes=[],
-             win_no_prefer_redirects=False,
-             win_private_assemblies=False,
-             cipher=block_cipher,
-             noarchive=False)
-pyz = PYZ(a.pure, a.zipped_data,
-             cipher=block_cipher)
-
-exe = EXE(pyz,
-          a.scripts,
-          a.binaries,
-          a.zipfiles,
-          a.datas,  
-          [],
-          name='main',
-          debug=False,
-          bootloader_ignore_signals=False,
-          strip=False,
-          upx=True,
-          upx_exclude=[],
-          runtime_tmpdir=None,
-          console=True,
-          disable_windowed_traceback=False,
-          target_arch=None,
-          codesign_identity=None,
-          entitlements_file=None , icon='university.ico')
->>>>>>> origin/main
